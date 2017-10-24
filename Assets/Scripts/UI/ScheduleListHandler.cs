49d2b08e69f7a43c056991d0096fb3e93780dae1
Andrew Bates <andrewbatesubc@gmail.com>
2017-10-24 13:02:19 -0700

    more work on canvas

--- Assets/Scripts/UI/ScheduleListHandler.cs
+++ Assets/Scripts/UI/ScheduleListHandler.cs
@@ -6,23 +6,50 @@ public class ScheduleListHandler : MonoBehaviour {
 
     public GameObject scheduleAnchor;
     public GameObject schedulePrefab;
-
+    public float verticalPadding = 32;
     private List<GameObject> scheduleList = new List<GameObject>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void AddSchedule(List<ScheduleDto> inboundSchedules) {
+        DestroySchedules();
+        GameObject previousSchedule = scheduleAnchor;
+        foreach(ScheduleDto dto in inboundSchedules) {
+            GameObject newSchedule = Instantiate(schedulePrefab, previousSchedule.GetComponent<RectTransform>().position, Quaternion.identity);
+            newSchedule.transform.SetParent(this.transform);
+            newSchedule.transform.localScale = previousSchedule.transform.localScale;
+            newSchedule.transform.localPosition = previousSchedule.transform.localPosition;
+            RectTransform trans = newSchedule.GetComponent<RectTransform>();
+            trans.localPosition = new Vector2(trans.localPosition.x, trans.localPosition.y - verticalPadding);
+
+            scheduleList.Add(newSchedule);
+            previousSchedule = newSchedule;
+        }
+    }
+
+    private void SetPositionAndScale() {
+
+    }
 
     public void AddSchedule() {
-        GameObject newSchedule = Instantiate(schedulePrefab, scheduleAnchor.GetComponent<RectTransform>().position, Quaternion.identity);
-        newSchedule.transform.parent = this.transform;
-        newSchedule.transform.localScale = scheduleAnchor.transform.localScale;
-        //newSchedule.GetComponent<RectTransform>().localPosition = scheduleAnchor.GetComponent<RectTransform>().localPosition;
+        ScheduleDto first = new ScheduleDto("andypandy", "midterm1", new string[0]);
+        ScheduleDto second = new ScheduleDto("andy    pandy", "regular", new string[0]);
+        ScheduleDto third = new ScheduleDto("bo bandyyyy", "midterm1", new string[0]);
+        AddSchedule(new List<ScheduleDto>() { first, second, third });
+    }
+
+    private void DestroySchedules() {
+        foreach (GameObject go in scheduleList) {
+            Destroy(go);
+        }
+        scheduleList = new List<GameObject>();
     }
 }
