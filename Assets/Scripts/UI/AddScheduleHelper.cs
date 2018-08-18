08197afb3f9cfbf2be27973c7e0c4d99e581652d
James Yoo <jyoo980@gmail.com>
2018-08-18 10:13:23 -0700

    Refactored AddScheduleHelper.cs

--- Assets/Scripts/UI/AddScheduleHelper.cs
+++ Assets/Scripts/UI/AddScheduleHelper.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AddScheduleHelper : MonoBehaviour {
-
+public class AddScheduleHelper : MonoBehaviour 
+{
 	public ServerAPI serverApi;
 	public Text newScheduleType;
 	public ScheduleTypeListHandler listHandler;
 
-	public void AddSchedule(){
-		string formattedScheduleType = newScheduleType.text.Trim ().ToLower ();
-		if (formattedScheduleType.Length == 0) {
-			return;
-		}
-		if (!listHandler.DoesContainGivenScheduleType (formattedScheduleType)) {
-			serverApi.AddScheduleType (formattedScheduleType, listHandler.AddScheduleTypes);
+	public void AddSchedule()
+	{
+		string formattedScheduleType = GetFormattedScheduleType(newScheduleType);
+		if (!ScheduleExists(formattedScheduleType) && formattedScheduleType != "")
+		{
+			serverApi.AddScheduleType(formattedScheduleType, listHandler.AddScheduleTypes);
 			newScheduleType.text = "";
 		}
+	}
+
+	private void GetFormattedScheduleType(Text scheduleType)
+	{
+		var formattedScheduleType = scheduleType.text.Trim().ToLower();
+		return (formattedScheduleType.Length != 0) ? formattedScheduleType : "";
+	}
+
+	private void ScheduleExists(string scheduleType)
+	{
+		return listHandler.DoesContainGivenScheduleType(scheduleType);
 	}
 }
